66c92f7c939804fa2033ea536d69e772bffdb293
Matt Ferry <mferry13@gmail.com>
2022-12-07 07:46:26 -0500

    WIP

--- src/types/Blueprint.ts
+++ src/types/Blueprint.ts
@@ -1,10 +1,10 @@
 import type { Dependency } from "@/types/Dependency";
 
 export class Blueprint {
-  name: string;
-  produces: number;
-  order: number;
-  required: Array<Dependency> = [];
+  private _name: string;
+  private _produces: number;
+  private _order: number;
+  private _required: Array<Dependency> = [];
 
   constructor(
     name: string,
@@ -12,9 +12,41 @@ export class Blueprint {
     order: number,
     required?: Array<Dependency> | null | undefined
   ) {
-    this.name = name;
-    this.produces = produces;
-    this.order = order;
-    this.required = required || [];
+    this._name = name;
+    this._produces = produces;
+    this._order = order;
+    this._required = required || [];
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get produces(): number {
+    return this._produces;
+  }
+
+  set produces(value: number) {
+    this._produces = value;
+  }
+
+  get order(): number {
+    return this._order;
+  }
+
+  set order(value: number) {
+    this._order = value;
+  }
+
+  get required(): Array<Dependency> {
+    return this._required;
+  }
+
+  set required(value: Array<Dependency>) {
+    this._required = value;
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,69 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+<script lang="ts">
+import { Dependency } from "@/types/Dependency";
+import { Blueprint } from "@/types/Blueprint";
+import blueprintConfig from "../assets/blueprints.json";
+
+export default {
+  data: () => ({
+    merchantCount: 6 as number,
+  }),
+  computed: {
+    blueprints(): Array<Blueprint> {
+      return blueprintConfig.map((config) => {
+        let requires: Array<Dependency> = [];
+        if (config.requires && config.requires.length) {
+          requires = config.requires.map(
+            (r) => new Dependency(r.name, r.count)
+          );
+        }
+        return new Blueprint(
+          config.name,
+          config.produces,
+          config.order,
+          requires
+        );
+      });
+    },
+    blueprintCounts(): Map<string, number> {
+      const countMap = new Map<string, number>();
+      this.blueprints.forEach((blueprint: Blueprint) => {
+        countMap.set(blueprint.name, this.getTotalCount(blueprint));
+      });
+      return countMap;
+    },
+  },
+  methods: {
+    getInheritedBlueprints(blueprint: Blueprint): Array<Blueprint> {
+      return this.blueprints.filter((bp: Blueprint) =>
+        bp.required.some((d: Dependency) => d.name === blueprint.name)
+      );
+    },
+    getTotalCount(blueprint: Blueprint): number {
+      let count = Math.ceil(this.merchantCount / blueprint.produces);
+      const inherited: Array<Blueprint> =
+        this.getInheritedBlueprints(blueprint);
+      if (inherited.length) {
+        count = 0;
+        this.getInheritedBlueprints(blueprint).forEach((bp: Blueprint) => {
+          const c = this.getTotalCount(bp);
+          const dep = bp.required.find(
+            (d: Dependency) => d.name === blueprint.name
+          );
+          if (dep) {
+            count += Math.ceil((c * dep.count) / blueprint.produces);
+          }
+        });
+      }
+      return count;
+    },
+  },
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div v-for="blueprint in blueprints" :key="blueprint.name">
+      {{ blueprint.name }} - {{ blueprintCounts.get(blueprint.name) }}
+    </div>
   </main>
 </template>
